9e2c1e3e02463ed022667b88a3c10f0494ecb584
fizzli <fizzli@163.com>
2019-07-31 21:41:09 +0800

    添加注释

--- src/main/java/com/fizzli/spring/boot/hello/world/controller/HelloWorldController.java
+++ src/main/java/com/fizzli/spring/boot/hello/world/controller/HelloWorldController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 返回字符串 "Hello World"
+ */
 @Controller
 public class HelloWorldController {
 
